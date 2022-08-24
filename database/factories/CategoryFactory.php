496eebd713c86bb8ac482cb5d5b7047f120cf909
Funke Faith Olasupo <fizzyflexy397@gmail.com>
2022-08-25 00:45:36 +0100

    category feature: Seed data for category

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = ucfirst(fake()->unique()->word);
+
+        return [
+            'uuid' => Str::uuid(),
+            'title' => $title,
+            'slug' => Str::slug($title,'-'),
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,9 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            BrandSeeder::class
+            BrandSeeder::class,
+            CategorySeeder::class,
+
         ]);
     }
 }
